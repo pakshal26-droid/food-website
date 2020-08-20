f596ae8de96918bd5a2822632e3be04ed1dc8b77
pakshal26-droid <68065022+pakshal26-droid@users.noreply.github.com>
2020-08-20 18:41:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+
+
+var  box= document.getElementById("Box1");
+function boxClicked(){
+    alert("We will teach you how to cook even for the Kings!");
+}
+
+box.addEventListener("click",boxClicked);
+
+var  box1= document.getElementById("Box2");
+function boxClicked(){
+    alert("We will teach you how to cook even for the Kings!");
+}
+
+box1.addEventListener("click",boxClicked);
+
+
+var  box3= document.getElementById("Box3");
+function boxClicked(){
+    alert("We will teach you how to cook even for the Kings!");
+}
+
+box3.addEventListener("click",boxClicked);
+
+/*var Games=["Fifa 18","Fortnite","Watch Dogs","Call of Duty"];
+console.log("I no longer play "+Games.pop()  + ".");
+console.log("But I still play "+ Games[1] + ".");
+console.log("I like football so I play", Games.shift());
+
+Games.forEach(function(i,ie){
+    console.log("I like to play", i,ie);
+})*/
+
+
+
+
+
+
+
